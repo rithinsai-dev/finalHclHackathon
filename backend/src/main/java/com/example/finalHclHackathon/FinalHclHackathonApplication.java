6da2dac001170a685735dfa61f3512e16a58d5c5
Pamerla Rithin <157945399+rithinsai-dev@users.noreply.github.com>
2026-05-25 09:25:12 +0530

    initialised the project with java and vite

---
+++ backend/src/main/java/com/example/finalHclHackathon/FinalHclHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalHclHackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalHclHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalHclHackathonApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/finalHclHackathon/FinalHclHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalHclHackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalHclHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
